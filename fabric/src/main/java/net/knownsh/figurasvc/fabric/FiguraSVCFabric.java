0d299e4d77bd4c4403182954edcb2933ce77d9f2
NIneroIN <82171594+NIneroIN@users.noreply.github.com>
2026-03-08 23:03:55 +0500

    Add VoicechatApi and sound event imports
    Added imports for VoicechatApi and ClientReceiveSoundEvent.

--- fabric/src/main/java/net/knownsh/figurasvc/fabric/FiguraSVCFabric.java
+++ fabric/src/main/java/net/knownsh/figurasvc/fabric/FiguraSVCFabric.java
@@ -2,7 +2,8 @@ package net.knownsh.figurasvc.fabric;
 
 import net.fabricmc.api.ModInitializer;
 import net.knownsh.figurasvc.FiguraSVC;
-
+import de.maxhenkel.voicechat.api.VoicechatApi;
+import de.maxhenkel.voicechat.api.events.ClientReceiveSoundEvent;
 /**
  * A mod class is not technically needed for Fabric to load the Plugin, but it's still nice to have.
  */
